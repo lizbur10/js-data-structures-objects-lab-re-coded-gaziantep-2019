577fa195d0861202e7adba314a393ecf4615d81f
Liz Burton <liz.burton147@gmail.com>
2019-09-07 00:19:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function deleteFromDriverByKey(driver, key) {
-  delete driver[key];
+  newDriver = Object.assign(driver);
+  delete newDriver[key];
+  return newDriver;
 }
